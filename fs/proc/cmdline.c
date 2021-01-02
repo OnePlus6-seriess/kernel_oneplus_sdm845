389cbe17f8d2d92ab8789ebd50f20ea2d8c80b8a
Erfan Abdi <erfangplus@gmail.com>
2021-01-02 18:55:27 +0330

    proc: Replace skip_initramfs to androidboot.force_normal_boot
    Change-Id: I6712cce574e6981577bed6718ad0b3daf57aee2a
    Signed-off-by: AnierinB <anierin@evolution-x.org>

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -47,6 +47,11 @@ static void proc_command_line_init(void) {
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "%s\n", proc_command_line);
+static char new_command_line[COMMAND_LINE_SIZE];
+
+static int cmdline_proc_show(struct seq_file *m, void *v)
+{
+	seq_printf(m, "%s\n", new_command_line);
 	return 0;
 }
 
@@ -65,6 +70,25 @@ static const struct file_operations cmdline_proc_fops = {
 static int __init proc_cmdline_init(void)
 {
 	proc_command_line_init();
+	char *offset_addr, *cmd = new_command_line;
+        char *search, *replace;
+	size_t search_len, replace_len;
+
+	strcpy(cmd, saved_command_line);
+
+	search = "skip_initramfs";
+	replace = "androidboot.force_normal_boot=1";
+	search_len = strlen(search);
+	replace_len = strlen(replace);
+
+	offset_addr = strstr(cmd, search);
+	if (offset_addr) {
+		size_t tail_len;
+		tail_len = strlen(offset_addr+search_len);
+
+		memmove(offset_addr+replace_len,offset_addr+search_len,tail_len+1);
+		memcpy(offset_addr, replace, replace_len);
+	}
 
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
